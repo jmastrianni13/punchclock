4ffcb93ae0baa7e490853bb99890e96ecb76ff3b
Joe Mastrianni <jmastrianni13@gmail.com>
2024-05-19 00:13:26 -0400

    added recorder

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,31 @@ use std::time::Instant;
 
 fn main() {
     println!("timer app");
-    let mut timer = Timer::new();
+    let mut recorder = Recorder::new();
     let mut command = String::new();
     loop {
         command.clear();
         io::stdin()
             .read_line(&mut command)
             .expect("Failed to read line");
-        match command.trim() {
-            "start" => timer.start(),
-            "stop" => {
-                let duration = timer.stop();
-                println!("{} seconds elapsed", duration);
+        let command_parts = command.trim().split(' ').collect::<Vec<_>>();
+        match command_parts {
+            command_parts if command_parts.len() == 1 => {
+                let c = command_parts[0];
+                match c {
+                    "stop" => recorder.stop_recording(),
+                    "show" => recorder.show_records(),
+                    "exit" => break,
+                    _ => println!("unknown command: {}", command.as_str()),
+                }
             }
-            "exit" => break,
+            command_parts if command_parts.len() == 2 => {
+                let (c1, c2) = (command_parts[0], command_parts[1]);
+                match c1 {
+                    "start" => recorder.start_recording(c2.to_string()),
+                    _ => println!("unknown command: {}", command.as_str()),
+                }
+            }
             _ => println!("unknown command: {}", command.as_str()),
         }
     }
@@ -56,5 +67,44 @@ impl Timer {
                 return 0 as u64;
             }
         }
+    }
+}
+
+#[derive(Debug)]
+struct Record {
+    duration: u64,
+    task: String,
+}
+
+struct Recorder {
+    timer: Timer,
+    records: Vec<Record>,
+    current_task: Option<String>,
+}
+
+impl Recorder {
+    fn new() -> Recorder {
+        return Recorder {
+            timer: Timer::new(),
+            records: Vec::new(),
+            current_task: None,
+        };
+    }
+
+    fn start_recording(&mut self, task: String) {
+        self.timer.start();
+        self.current_task = Some(task);
+    }
+
+    fn stop_recording(&mut self) {
+        if let Some(task) = self.current_task.take() {
+            let duration = self.timer.stop();
+            let record = Record { duration, task };
+            self.records.push(record);
+        }
+    }
+
+    fn show_records(&self) {
+        println!("{:?}", self.records);
     }
 }
